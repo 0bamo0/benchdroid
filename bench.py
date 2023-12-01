aa39f876df68dd8fb3f7fb8ae6cf38e82ae5fcfe
Amo <amokrane123123@gmail.com>
2023-12-01 22:34:41 +0100

    added history file

--- bench.py
+++ bench.py
@@ -4,5 +4,11 @@ startTime = time.time()
 i = 0
 while(i < 10000000):
     i = i + 1
+result = time.time() - startTime
 
-print(time.time()-startTime)
+file_path = "results.txt"
+
+with open(file_path, "a") as file:
+    file.write(str(result) + "\n")
+
+print(f"Result appended to {file_path}")
